cd0ab49aeb4253fc4c4edf4104f8d013b163a716
Alex Cook <apcook7104@gmail.com>
2022-11-30 21:30:23 -0800

    refactor how to run daily solutions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,34 @@
+use std::{fs::File, io, path::Path};
 
+use anyhow::bail;
+
+pub mod day1;
+
+/// Runs a solution from a given `day`.
+pub fn solution(day: u8) -> anyhow::Result<()> {
+    let input_filepath = format!("input/day_{day}.txt");
+    let input = match read_input_file(input_filepath) {
+        Ok(s) => s,
+        Err(e) => {
+            bail!("failed to read input file for day {day}: {e}");
+        }
+    };
+
+    match day {
+        1 => day1::main(input),
+        d => bail!("solution for day {d} does not exist"),
+    }
+}
+
+/// Reads challenge input files.
+fn read_input_file(path: String) -> Result<String, io::Error> {
+    log::debug!("input file path: {path}");
+
+    let path = Path::new(&path);
+    let file = File::open(path)?;
+
+    let content = io::read_to_string(file)?;
+    log::debug!("input file content: {content}");
+
+    Ok(content)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use std::{fs::File, io, path::Path};
-
 use clap::Parser;
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
 /// The default day to execute if CLI argument is not given.
 const DEFAULT_DAY: u8 = 1;
-
-/// The directory from which to read challenge input files.
-const INPUT_FILE_DIRECTORY: &str = "input";
 
 /// CLI for running Advent of Code challenges.
 #[derive(Parser)]
@@ -18,20 +13,7 @@ struct AdventOfCode {
     day: Option<u8>,
 }
 
-/// Reads challenge input files.
-fn read_input_file(path: String) -> Result<String, io::Error> {
-    log::info!("input file path: {path}");
-
-    let path = Path::new(&path);
-    let file = File::open(path)?;
-
-    let content = io::read_to_string(file)?;
-    log::debug!("input file content: {content}");
-
-    Ok(content)
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> anyhow::Result<()> {
     SimpleLogger::new()
         .with_level(LevelFilter::Info)
         .env()
@@ -40,22 +22,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let aoc = AdventOfCode::parse();
     let day = match aoc.day.unwrap_or(DEFAULT_DAY) {
         0 => {
-            log::error!("`day` argument cannot be 0, using 1 instead");
+            log::error!("--day argument cannot be 0, using 1 instead");
             1
         }
         x => x,
     };
 
-    log::info!("running challenge from day {day}");
+    log::info!("running solution for day {day}");
 
-    let input_filepath = format!("{INPUT_FILE_DIRECTORY}/day_{day}.txt");
-    let input = match read_input_file(input_filepath) {
-        Ok(s) => s,
-        Err(e) => {
-            log::error!("failed to read input file: {e}");
-            return Err(Box::new(e));
-        }
-    };
+    advent_of_code_2022::solution(day)?;
 
     Ok(())
 }
