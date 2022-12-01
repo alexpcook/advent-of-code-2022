638a628b961a78ac5d998e5c7c8c9d4d8d186768
Alex Cook <apcook7104@gmail.com>
2022-11-30 21:00:59 -0800

    add reading of input files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::{fs::File, io, path::Path};
+
 use clap::Parser;
 use simple_logger::SimpleLogger;
 
 /// The default day to execute if CLI argument is not given.
 const DEFAULT_DAY: u8 = 1;
+
+/// The directory from which to read challenge input files.
+const INPUT_FILE_DIRECTORY: &str = "input";
 
 /// CLI for running Advent of Code challenges.
 #[derive(Parser)]
@@ -10,6 +15,19 @@ const DEFAULT_DAY: u8 = 1;
 struct AdventOfCode {
     #[arg(short, long)]
     day: Option<u8>,
+}
+
+/// Reads challenge input files.
+fn read_input_file(path: String) -> Result<String, io::Error> {
+    log::info!("input file path: {path}");
+
+    let path = Path::new(&path);
+    let file = File::open(path)?;
+
+    let content = io::read_to_string(file)?;
+    log::debug!("input file content: {content}");
+
+    Ok(content)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,6 +40,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             1
         }
         x => x,
+    };
+
+    let input_filepath = format!("{INPUT_FILE_DIRECTORY}/day_{day}.txt");
+    let input = match read_input_file(input_filepath) {
+        Ok(s) => s,
+        Err(e) => {
+            log::error!("failed to read input file: {e}");
+            return Err(Box::new(e));
+        }
     };
 
     log::info!("running challenge from day {day}");
