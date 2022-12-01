d2324e8a286789ad0fc9450e1ba6019df49a5bcb
Alex Cook <apcook7104@gmail.com>
2022-11-30 20:33:51 -0800

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use simple_logger::SimpleLogger;
+
+/// The default day to execute if CLI argument is not given.
+const DEFAULT_DAY: u8 = 1;
+
+/// CLI for running Advent of Code challenges.
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct AdventOfCode {
+    #[arg(short, long)]
+    day: Option<u8>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new().init()?;
+
+    let aoc = AdventOfCode::parse();
+    let day = match aoc.day.unwrap_or(DEFAULT_DAY) {
+        0 => {
+            log::error!("`day` argument cannot be 0, using 1 instead");
+            1
+        }
+        x => x,
+    };
+
+    log::info!("running challenge from day {day}");
+
+    Ok(())
 }
