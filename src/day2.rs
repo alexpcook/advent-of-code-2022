3c97f5c283cc274bfc12f0910e6aa59b2dfe3f45
Alex Cook <apcook7104@gmail.com>
2022-12-01 22:28:59 -0800

    add doc comments to solution

--- src/day2.rs
+++ src/day2.rs
@@ -17,6 +17,7 @@ pub fn main(input: String) -> anyhow::Result<()> {
     Ok(())
 }
 
+/// Part 1 solution.
 fn part1(games: &[(char, char)]) -> u64 {
     games
         .iter()
@@ -32,6 +33,7 @@ fn part1(games: &[(char, char)]) -> u64 {
         .sum()
 }
 
+/// Part 2 solution.
 fn part2(games: &[(char, char)]) -> u64 {
     games
         .iter()
