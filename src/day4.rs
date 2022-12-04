e9b4a6f2959775f506e1acf59658f8638701d9cb
Alex Cook <apcook7104@gmail.com>
2022-12-03 22:00:06 -0800

    enhance day 4 solution

--- src/day4.rs
+++ src/day4.rs
@@ -1,35 +1,47 @@
 pub fn main(input: String) -> anyhow::Result<()> {
     let pairs: Vec<((u32, u32), (u32, u32))> = input
         .lines()
-        .map(|l| {
-            let (first, second) = l.split_once(',').unwrap();
-            let (start1, end1) = first.split_once('-').unwrap();
-            let (start2, end2) = second.split_once('-').unwrap();
-            (
-                (start1.parse().unwrap(), end1.parse().unwrap()),
-                (start2.parse().unwrap(), end2.parse().unwrap()),
-            )
+        .map(|line| {
+            line.split_once(',')
+                .and_then(|(first_range, second_range)| {
+                    first_range
+                        .split_once('-')
+                        .and_then(|(start, end)| start.parse().ok().zip(end.parse().ok()))
+                        .zip(
+                            second_range
+                                .split_once('-')
+                                .and_then(|(start, end)| start.parse().ok().zip(end.parse().ok())),
+                        )
+                })
+                .unwrap_or_default()
         })
         .collect();
 
-    let mut fully_contained_count = 0;
-    let mut any_overlap_count = 0;
-
-    for ((start1, end1), (start2, end2)) in pairs {
-        if (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1) {
-            fully_contained_count += 1;
-        }
-
-        if (end1 >= start2 && start1 <= start2) || (end2 >= start1 && start2 <= start1) {
-            any_overlap_count += 1;
-        }
-    }
-
     // Part 1
+    let fully_contained_count = pairs
+        .clone()
+        .into_iter()
+        .filter(one_range_is_fully_contained)
+        .count();
     log::info!("full contained count: {fully_contained_count}");
 
     // Part 2
-    log::info!("any overlap: {any_overlap_count}");
+    let any_overlap_count = pairs.into_iter().filter(ranges_have_any_overlap).count();
+    log::info!("any overlap count: {any_overlap_count}");
 
     Ok(())
+}
+
+/// Is one range fully contained within the other?
+fn one_range_is_fully_contained(ranges: &((u32, u32), (u32, u32))) -> bool {
+    let lhs = ranges.0;
+    let rhs = ranges.1;
+    (lhs.0 <= rhs.0 && lhs.1 >= rhs.1) || (rhs.0 <= lhs.0 && rhs.1 >= lhs.1)
+}
+
+/// Do the ranges overlap at all?
+fn ranges_have_any_overlap(ranges: &((u32, u32), (u32, u32))) -> bool {
+    let lhs = ranges.0;
+    let rhs = ranges.1;
+    (rhs.0 <= lhs.0 || lhs.1 >= rhs.0) && lhs.0 <= rhs.1
 }
