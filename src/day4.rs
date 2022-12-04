43e5bc23886b45752b41dcdc0186be7b28577fe2
Alex Cook <apcook7104@gmail.com>
2022-12-03 21:23:40 -0800

    initial day 4 solution

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,35 @@
 pub fn main(input: String) -> anyhow::Result<()> {
-    todo!();
+    let pairs: Vec<((u32, u32), (u32, u32))> = input
+        .lines()
+        .map(|l| {
+            let (first, second) = l.split_once(',').unwrap();
+            let (start1, end1) = first.split_once('-').unwrap();
+            let (start2, end2) = second.split_once('-').unwrap();
+            (
+                (start1.parse().unwrap(), end1.parse().unwrap()),
+                (start2.parse().unwrap(), end2.parse().unwrap()),
+            )
+        })
+        .collect();
+
+    let mut fully_contained_count = 0;
+    let mut any_overlap_count = 0;
+
+    for ((start1, end1), (start2, end2)) in pairs {
+        if (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1) {
+            fully_contained_count += 1;
+        }
+
+        if (end1 >= start2 && start1 <= start2) || (end2 >= start1 && start2 <= start1) {
+            any_overlap_count += 1;
+        }
+    }
+
+    // Part 1
+    log::info!("full contained count: {fully_contained_count}");
+
+    // Part 2
+    log::info!("any overlap: {any_overlap_count}");
+
+    Ok(())
 }
