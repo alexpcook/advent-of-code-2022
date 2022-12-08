7ab1e44f3c45df97ebe5a4646616c5619ddad4fe
Alex Cook <apcook7104@gmail.com>
2022-12-07 22:42:22 -0800

    add day 8 part 2 solution

--- src/day8.rs
+++ src/day8.rs
@@ -1,11 +1,11 @@
 pub fn main(input: String) -> anyhow::Result<()> {
-    let mut visible_trees = 0;
-    let mut highest_scenic_score = 0;
-
     let forest: Vec<Vec<u32>> = input
         .lines()
         .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect())
         .collect();
+
+    // Part 1
+    let mut visible_trees = 0;
 
     let rows = forest.len();
 
@@ -23,12 +23,10 @@ pub fn main(input: String) -> anyhow::Result<()> {
                 continue;
             }
 
-            let current_height = forest.get(i).unwrap().get(j).unwrap();
-
             // Up
             if (0..i).all(|n| {
                 let other_height = forest.get(n).unwrap().get(j).unwrap();
-                other_height < current_height
+                other_height < tree
             }) {
                 visible_trees += 1;
                 continue;
@@ -37,7 +35,7 @@ pub fn main(input: String) -> anyhow::Result<()> {
             // Down
             if (i + 1..rows).all(|n| {
                 let other_height = forest.get(n).unwrap().get(j).unwrap();
-                other_height < current_height
+                other_height < tree
             }) {
                 visible_trees += 1;
                 continue;
@@ -46,7 +44,7 @@ pub fn main(input: String) -> anyhow::Result<()> {
             // Left
             if (0..j).all(|n| {
                 let other_height = forest.get(i).unwrap().get(n).unwrap();
-                other_height < current_height
+                other_height < tree
             }) {
                 visible_trees += 1;
                 continue;
@@ -55,7 +53,7 @@ pub fn main(input: String) -> anyhow::Result<()> {
             // Right
             if (j + 1..cols).all(|n| {
                 let other_height = forest.get(i).unwrap().get(n).unwrap();
-                other_height < current_height
+                other_height < tree
             }) {
                 visible_trees += 1;
                 continue;
@@ -63,10 +61,77 @@ pub fn main(input: String) -> anyhow::Result<()> {
         }
     }
 
-    // Part 1
     log::info!("number of trees visible from outside the forest: {visible_trees}");
 
     // Part 2
+    let mut highest_scenic_score = 0;
+
+    let rows = forest.len();
+
+    for (i, row) in forest.iter().enumerate() {
+        let cols = row.len();
+
+        for (j, tree) in row.iter().enumerate() {
+            // Up
+            let up_score = if i == 0 {
+                0
+            } else {
+                match (0..i).position(|n| {
+                    let other_height = forest.get(i - n - 1).unwrap().get(j).unwrap();
+                    other_height >= tree
+                }) {
+                    Some(blocking_tree_iter_pos) => blocking_tree_iter_pos + 1,
+                    None => i,
+                }
+            };
+
+            // Down
+            let down_score = if i == rows - 1 {
+                0
+            } else {
+                match (i + 1..rows).position(|n| {
+                    let other_height = forest.get(n).unwrap().get(j).unwrap();
+                    other_height >= tree
+                }) {
+                    Some(blocking_tree_iter_pos) => blocking_tree_iter_pos + 1,
+                    None => rows - i - 1,
+                }
+            };
+
+            // Left
+            let left_score = if j == 0 {
+                0
+            } else {
+                match (0..j).position(|n| {
+                    let other_height = forest.get(i).unwrap().get(j - n - 1).unwrap();
+                    other_height >= tree
+                }) {
+                    Some(blocking_tree_iter_pos) => blocking_tree_iter_pos + 1,
+                    None => j,
+                }
+            };
+
+            // Right
+            let right_score = if j == cols - 1 {
+                0
+            } else {
+                match (j + 1..cols).position(|n| {
+                    let other_height = forest.get(i).unwrap().get(n).unwrap();
+                    other_height >= tree
+                }) {
+                    Some(blocking_tree_iter_pos) => blocking_tree_iter_pos + 1,
+                    None => cols - j - 1,
+                }
+            };
+
+            let scenic_score = up_score * down_score * left_score * right_score;
+
+            if scenic_score > highest_scenic_score {
+                highest_scenic_score = scenic_score;
+            }
+        }
+    }
+
     log::info!("highest scenic score in the forest: {highest_scenic_score}");
 
     Ok(())
